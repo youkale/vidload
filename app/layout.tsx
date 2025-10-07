1ac0fbf5d9377a48134432b44fc2a50035be61ca
youkale <tdycxs@gmail.com>
2025-10-07 22:51:17 +0800

    feat: update clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -112,6 +112,19 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t8njom49n3");
+            `,
+          }}
+        />
       </head>
       <body className={inter.className} suppressHydrationWarning>
         {children}
